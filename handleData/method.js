219c9fffb1919344c7c19fb0d84f0c086e3fab49
woody-park <starface1009@gmail.com>
2022-02-07 22:08:06 +0900

    기존 fitUsedBookData의 용도변경과 명칭변경

--- handleData/method.js
+++ handleData/method.js
@@ -23,177 +23,81 @@ const fileNameLive = function () {
 
 
 
-// yes24랑 알라딘 api 맞추기
-
-
-const yes24 = [
-    {
-        "id": 0,
-        "link": "http://test.com//",
-        "img": "https://image.test.com/",
-        "title": "[도서] 테스트",
-        "author": [
-            "테스트 작가",
-            " 테스트 출판사 ",
-            " 2021년 11월"
-        ],
-        "isbn": [
-            "ISBN13 - 1234567890xxx ",
-            " ISBN10 - 0987654321"
-        ],
-        "priceText": [
-            "정가",
-            "매입가(최상)",
-            "매입가(상)",
-            "매입가(중)\n"
-        ],
-        "price": [
-            "17,800원",
-            "7,200원",
-            "6,500원",
-            "5,700원"
-        ]
-    }
-]
-
-const aladin = [
-    {
-        "id": 0,
-        "link": "http://www.test.co.kr/shop",
-        "cover": "https://image.test.co.kr/product",
-        "title": "자바스크립트1",
-        "pubDate": "2021-01-22",
-        "author": "지은이",
-        "isbn": "K1327xcsd5",
-        "isbn13": "abcderfs32212",
-        "subInfo": {
-            "AA": 7500,
-            "A": 6800,
-            "B": 6000,
-            "C": 0
-        }
-    },
-    {
-        "id": 1,
-        "link": "http://www.test.co.kr/shop",
-        "cover": "https://image.test.co.kr/product",
-        "title": "자바스크립트2",
-        "pubDate": "2021-01-22",
-        "author": "지은이",
-        "isbn": "K1327xcsd5",
-        "isbn13": "abcderfs32212",
-        "subInfo": {
-            "AA": 8500,
-            "A": 4800,
-            "B": 3000,
-            "C": 0
-        }
-    }
-]
+// 알라딘 api 데이터 subInfo 수정을 위한 메서드
 
 
 
 // 숫자에 한국 화폐 기준으로 콤마찍기
 const makeComma = function (arr) {
-
     let priceComma = [];
 
-
-    for (var m of arr) {
-
+    for (let m of arr) {
         let money = m.toLocaleString('ko-KR');
         priceComma.push(money.toString() + "원");
     }
-
     return priceComma;
 }
 
 
 
+//// 각각의 데이터 모두 중간에서 바꾸기 보다는 가지고온 객체들을 여기서 핸들링하여 하나의 데이터로 합치는게 낫다고 판단
+
+//! 생각해보니 같은 책으로 비교하는 건데.. 굳이 이걸 통합할 필요가??
+//! 우리가 구분해야하는 것은 가격 정도면 충분! 정확히는 중고가격과 중고 매입가
+//! 나중에 어느쪽이던 데이터만 필요한 것만 뽑아 쓰면된다 왜? 같은 책이라 isbn번호랑 출판정보가 모두 같다 !!! 
+//! 이거로 며칠 고민했는데 멍청한 짓을 한 것 같다.. 그래도 이것 저것 시도해보면서 감각을 찾고 있다.
 
 
+const handleAladinData = function (obj) {
 
-const fitUsedBookData = function (obj1, obj2) {
-
-    //let priceTextInfo = Object.keys(obj1[0].subInfo); // 정가, 매입가..
-    //let priceInfo = Object.values(obj1[0].subInfo); // 10000원, 5000원...
-
-    //let priceText = [];
-    // let price = [];
-
-
-    // for (let key in obj1) {
-    //     console.log("key", obj1[key]['subInfo']);
-    // }
-
-
-
+    // ? obj1 : aladin 객체가 들어올경우 
+    // ? -> 고칠 것 subInfo의 텍스트와 값을 나눈다
+    // ? -> 값에는 한국식으로 콤마와 '원'이 들어간다 
 
     // obj1에서 subInfo의 키와 값 분리 작업
-    for (let info of obj1) {
-        var subInfo = info['subInfo'];
-    }
-    console.log("subInfo: ", subInfo); // { AA: 8500, A: 4800, B: 3000, C: 0 }
+    // for (let info of obj1) {
+    //     var subInfo = info['subInfo'];
+    // }
+    // console.log("subInfo: ", subInfo); // { AA: 8500, A: 4800, B: 3000, C: 0 }
 
 
 
-    // let priceTextInfo = Object.keys(subInfo);
-    // let priceInfo = Object.values(subInfo)
-
-
-    //console.log("priceTextInfo: ", priceTextInfo);       // [ 'AA', 'A', 'B', 'C' ]
-    //console.log("priceInfo: ", priceInfo);      // [ 8500, 4800, 3000, 0 ]
-
-
-    // obj1.priceText = priceTextInfo;
-    //obj1.price = priceInfo;
-    //delete obj1[0].subInfo;
-
-
-    let arr = [];
-
-    for (let i = 0; i < obj1.length; i++) {
+    for (let i = 0; i < obj.length; i++) {
         // console.log("포문", obj1[i]['subInfo']) // { AA: 7500, A: 6800, B: 6000, C: 0 }
 
-        var priceTextInfo = Object.keys(subInfo);
-        var priceInfo = Object.values(subInfo);
+        var priceTextInfo = Object.keys(obj[i]['subInfo']);
+        var priceInfo = Object.values(obj[i]['subInfo']);
         let priceText = priceTextInfo;//[ 'AA', 'A', 'B', 'C' ]
         priceText.splice(0, 4, "정가", "매입가(최상)", "매입가(상)", "매입가(중)");
         //var price = priceInfo; // [ 8500, 4800, 3000, 0 ]
 
-        var price = makeComma(priceInfo);
 
-        console.log("여기", price)
+        var price = makeComma(priceInfo);   // 숫자에 콤마와 문자열을 더해주기 위한 메소드 실행
+
+        // console.log("여기", price)
 
 
+        obj[i].priceText = priceText;
+        obj[i].price = price; // [ 8500, 4800, 3000, 0 ]
 
-
-        obj1[i].priceText = priceText;
-        obj1[i].price = price; // [ 8500, 4800, 3000, 0 ]
-
-        delete obj1[i].subInfo; // 기존에 남은 subInfo를 지운다
+        delete obj[i].subInfo; // 기존에 남은 subInfo를 지운다
     }
 
-    console.log("결과:", obj1)
-
-
-    // for (let key in c) {
-    //     priceText.push(key);
-    //     price.push(c[key]);
-    // }
-    //console.log("323", priceText);
-    //console.log("123", price);
+    //console.log("결과:", obj);
+    // 최종적으로 변환 된 obj;
+    return obj;
 
 }
 
+//fitUsedBookData(aladin, yes24);
 
-fitUsedBookData(aladin, yes24);
-
+//handleAladinData(aladin);
 
 
 
 
 
 module.exports = {
-    fileNameLive
+    fileNameLive,
+    handleAladinData
 }
