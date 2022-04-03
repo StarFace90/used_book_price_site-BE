d9768d0eb52d350d03d5919972ab7d82d7f7a72a
woody-park <starface1009@gmail.com>
2022-04-03 23:58:18 +0900

    쿼리문 연결을 위한 함수 생성 및 크롤링 함수 변경

--- handleData/yes24Data.js
+++ handleData/yes24Data.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
 const scrap = require('puppeteer');
+const fs = require('fs');
 const toUnicode = require('./decode');
 const file = require('./method');
 
@@ -7,123 +7,158 @@ const file = require('./method');
 
 // decode.js에서 가져온 charToUnicode 함수 사용
 // ? 지금은 문자를 직접 넣었지만, 추후 프런트에서 보낼 데이터를 받아 사용
-let keyword = toUnicode.charToUnicode('해리 포터');
+
 // 띄어쓰기에도 결과 잘 나옴
 
+async function getQueryFromClientToYes24(obj) {
 
-// app.js로 보내기 위한 모듈화
-module.exports =
-    (async () => {
-        const browser = await scrap.launch(); // headless 브라우저 실행
-        const page = await browser.newPage(); // 새로운 페이지 열기 -> yes24 중고 매입 페이지
-
-        // 직접 특정 값을 줘서 데이터 잘 오는지 확인 (여기서는 검색어 해리포터)
-        //! 문제 실제 사이트에서 한글로 검색시 한글 키워드가 디코딩됨 해결책 모색!
-        //await page.goto(`http://www.yes24.com/Mall/buyback/Search?CategoryNumber=018&SearchWord=%uBCA0%uB974%uB098%uB974%20%uBCA0%uB974%uBCA0%uB974&SearchDomain=BOOK,FOREIGN&BuybackAccept=N`)
-
-        await page.goto(`http://www.yes24.com/Mall/buyback/Search?CategoryNumber=018&SearchWord=${keyword}&SearchDomain=BOOK,FOREIGN&BuybackAccept=N`)
-
-
-        // usedBooks을 선언하여 전체 데이터 가져온다.
-        const usedBooks = await page.evaluate(() => {
-            let scrappedData = []; // 스크랩된 내용을 담을 빈 배열
-
-            let yes24Data = document.querySelector('div.bbGoodsList').children[0].children;
-
-            for (let i = 0; i < yes24Data.length; i++) {
-
-
-                // link : 상품 페이지로 가는 링크
-                // img : 상품 이미지
-                // title : 상품 이름
-                // author : 저자 
-                // publishDate : 출판 날짜 
-                // isbn13 : isbn 넘버 
-                // isbn10 : isbn 넘버 
-                // priceList : 정가
-                // priceHighest : 매입가(최상)
-                // pricehHigh : 매입가(상)
-                // priceMiddle : 매입가(중)  
-
-                let priceText = yes24Data[i].querySelector('div.bbG_price').children[0].children[2].innerText.split('\t');
-                let price = yes24Data[i].querySelector('div.bbG_price').children[0].children[3].innerText.split('\t');
+    let dataQuery = await obj;
+    console.log("yes24키워드", dataQuery);
+    let keyword = await toUnicode.charToUnicode(dataQuery);
+    let ccc = testApi(keyword);
+    return ccc;
+}
 
 
 
-                // 위에서 선언한 빈배열 안에 가공한 데이터들을 넣는다.
-                scrappedData.push({
-                    //normal: tbodyChilds.children[0].textContent
-                    id: i,
-                    link: yes24Data[i].firstElementChild.children[0].href,
-                    img: yes24Data[i].firstElementChild.childNodes[1].children[0].children[0].src,
-                    title: yes24Data[i].querySelector('p.bbG_name').innerText,
+// app.js로 보내기 위한 모듈화 
 
-                    // author 배열형태 [0]부터 순서대로 저자, 출판사, 출판년월
-                    author: yes24Data[i].querySelector('p.bbG_pubGrp').innerText.split('|'),
-                    isbn: yes24Data[i].querySelector('p.bbG_isbn').innerText.split('|'),
+const testApi = async (keyword) => {
 
-                    priceText: priceText,
-                    price: price,
+    console.log("testAPi키워드", keyword);
+    const browser = await scrap.launch(); // headless 브라우저 실행
+    const page = await browser.newPage(); // 새로운 페이지 열기 -> yes24 중고 매입 페이지
 
-                });
-            }
-            return scrappedData;
-
-            //scrappedData.push()
-
-        })
-        // console.log('priceText', priceText);
-        //console.log('price', price);
-        //console.log("결과값", usedBooks);
-
-        //! 아래와 같이 데이터를 담아온다.
-        /**
-         * ? 가져온 데이터를 db에 담을 방법
-         * ? 위에 언급된 문제 되는 디코딩 해결법 모색하기
-         * ? 급하게 하느라 코드나 주석이 지저분하다 간결화 하는 방법 생각
-         * ? 실제 홈페이지에서는 한글, 영어, 띄어쓰기, isbn 으로도 검색이 가능하다 
-         */
-        // {
-        //     id: 18,
-        //     link: 'http://www.yes24.com/Product/Goods/82941827',
-        //     img: 'http://image.yes24.com/goods/82941827/M',
-        //     title: '[도서] 해리포터와 불의 잔 3',
-        //     author: [ 'J.K. 롤링 저/강동혁 역 ', ' 문학수첩 ', ' 2019년 11월' ],
-        //     isbn: [ 'ISBN13 - 9788983927705', 'ISBN10 - 8983927704' ],
-        //     priceText: [ '정가', '매입가(최상)', '매입가(상)', '매입가(중)\n' ],
-        //     price: [ '9,500원', '2,900원', '2,600원', '2,300원' ]
-        //   },
-
-        // ? api 요청시 
+    // 직접 특정 값을 줘서 데이터 잘 오는지 확인 (여기서는 검색어 해리포터)
+    //! 문제 실제 사이트에서 한글로 검색시 한글 키워드가 디코딩됨 해결책 모색!
+    //await page.goto(`http://www.yes24.com/Mall/buyback/Search?CategoryNumber=018&SearchWord=%uBCA0%uB974%uB098%uB974%20%uBCA0%uB974%uBCA0%uB974&SearchDomain=BOOK,FOREIGN&BuybackAccept=N`)
+    await page.goto(`http://www.yes24.com/Mall/buyback/Search?CategoryNumber=018&SearchWord=${keyword}&SearchDomain=BOOK,FOREIGN&BuybackAccept=N`);
 
 
-        // 크롤링한 데이터와 api 요청 데이터 통합 전에 file system 모듈로 json 파일 생성 후 저장 테스트 
-        // 성공시 json파일을 이용하여 db에 저장하는 방법도 모색 또는 프런트 요청에 의한 쿼리로 db에 저장하는 방법 모색
+    // usedBooks을 선언하여 전체 데이터 가져온다.
+    const usedBooks = await page.evaluate(() => {
+        let scrappedData = []; // 스크랩된 내용을 담을 빈 배열
+
+        let yes24Data = document.querySelector('div.bbGoodsList').children[0].children;
+
+        for (let i = 0; i < yes24Data.length; i++) {
+
+
+            // link : 상품 페이지로 가는 링크
+            // img : 상품 이미지
+            // title : 상품 이름
+            // author : 저자 
+            // publishDate : 출판 날짜 
+            // isbn13 : isbn 넘버 
+            // isbn10 : isbn 넘버 
+            // priceList : 정가
+            // priceHighest : 매입가(최상)
+            // pricehHigh : 매입가(상)
+            // priceMiddle : 매입가(중)  
+
+            let priceText = yes24Data[i].querySelector('div.bbG_price').children[0].children[2].innerText.split('\t');
+            let price = yes24Data[i].querySelector('div.bbG_price').children[0].children[3].innerText.split('\t');
 
 
 
-        // 파일명 모듈
-        const fileName = file.fileNameLive();
+            // 위에서 선언한 빈배열 안에 가공한 데이터들을 넣는다.
+            scrappedData.push({
+
+                id: i,
+                link: yes24Data[i].firstElementChild.children[0].href,
+                img: yes24Data[i].firstElementChild.childNodes[1].children[0].children[0].src,
+                title: yes24Data[i].querySelector('p.bbG_name').innerText,
+
+                // author 배열형태 [0]부터 순서대로 저자, 출판사, 출판년월
+                author: yes24Data[i].querySelector('p.bbG_pubGrp').innerText.split('|'),
+                isbn: yes24Data[i].querySelector('p.bbG_isbn').innerText.split('|'),
+
+                priceText: priceText,
+                price: price,
+
+            });
+        }
+        return scrappedData;
+
+        //scrappedData.push()
+
+    });
+
+
+    fs.writeFile(`Docs/yes24Data.json`,
+        JSON.stringify(usedBooks, null, 2), 'utf-8',
+        err =>
+            err ? console.error('yes24파일 생성에 실패했습니다', err)
+                : console.log('yes24 파일 생성에 성공했습니다!')
+    );
+    // let writefileContents = JSON.parse(JSON.stringify(usedBooks));
+    // return writefileContents;
+
+    await browser.close();
+    //console.log('브라우저 닫기');
+
+};
+// console.log('priceText', priceText);
+//console.log('price', price);
+//console.log("결과값", usedBooks);
+
+//! 아래와 같이 데이터를 담아온다.
+/**
+ * ? 가져온 데이터를 db에 담을 방법
+ * ? 위에 언급된 문제 되는 디코딩 해결법 모색하기
+ * ? 급하게 하느라 코드나 주석이 지저분하다 간결화 하는 방법 생각
+ * ? 실제 홈페이지에서는 한글, 영어, 띄어쓰기, isbn 으로도 검색이 가능하다 
+ */
+// {
+//     id: 18,
+//     link: 'http://www.yes24.com/Product/Goods/82941827',
+//     img: 'http://image.yes24.com/goods/82941827/M',
+//     title: '[도서] 해리포터와 불의 잔 3',
+//     author: [ 'J.K. 롤링 저/강동혁 역 ', ' 문학수첩 ', ' 2019년 11월' ],
+//     isbn: [ 'ISBN13 - 9788983927705', 'ISBN10 - 8983927704' ],
+//     priceText: [ '정가', '매입가(최상)', '매입가(상)', '매입가(중)\n' ],
+//     price: [ '9,500원', '2,900원', '2,600원', '2,300원' ]
+//   },
+
+// ? api 요청시 
+
+
+// 크롤링한 데이터와 api 요청 데이터 통합 전에 file system 모듈로 json 파일 생성 후 저장 테스트 
+// 성공시 json파일을 이용하여 db에 저장하는 방법도 모색 또는 프런트 요청에 의한 쿼리로 db에 저장하는 방법 모색
 
 
 
-        // usedBooks로 데이터 크롤링 한 것을 저장한다
-        fs.writeFile(`Docs/yes24Data-${fileName}.json`,
-            JSON.stringify(usedBooks, null, 2), 'utf-8',
-            err =>
-                err ? console.error('파일 생성에 실패했습니다', err)
-                    : console.log('파일 생성에 성공했습니다!')
-        );
+// 파일명 모듈
+// const fileName = file.fileNameLive();
+
+// page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
+
+// await page.evaluate(() => console.log(`url is ${location.href}`));
+
+// usedBooks로 데이터 크롤링 한 것을 저장한다
+
+// await fs.writeFile(`Docs/yes24Data.json`,
+//     JSON.stringify(usedBooks, null, 2), 'utf-8',
+//     (err) => {
+//         if (err) throw err;
+//         console.log('yes24 파일 생성에 성공했습니다!')
+//     }
+// );
+// fs.writeFile('./meal.json', JSON.stringify(usedBooks), (err) => {
+//     if (err) throw err;
+//     console.log('OK');
+// });
+
+// 파일 생성된 내용 보여주는 콘솔
+// const writefileContents = JSON.parse(JSON.stringify(usedBooks));
+// console.log(writefileContents);
+
+//await page.waitForTimeout(100);
 
 
-        // 파일 생성된 내용 보여주는 콘솔
-        let writefileContents = JSON.parse(JSON.stringify(usedBooks));
-        console.log(writefileContents);
 
 
 
-        // 브라우저 닫기 
-        await browser.close();
-
-
-    })();
+module.exports = {
+    getQueryFromClientToYes24
+}
