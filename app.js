44bdc86a388658fcaff6111e101c9b1cbbb80f99
woody-park <starface1009@gmail.com>
2022-04-04 00:03:32 +0900

    json파일 생성 및 내용 삭제를  위한 미들웨어 및 함수  추가

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 //const test = require('./test');
-const { getQueryFromClient } = require('./handleData/aladinApi');
+
+const dataHandler = require('./handleData/dataHandle');
+const { next } = require('cheerio/lib/api/traversing');
 
 const app = express();
 
@@ -23,6 +25,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(morgan('dev'))
+
+
+// const fileLenAl = fs.readFileSync('Docs/aladinApi.json', 'utf-8');
+// const fileLen24 = fs.readFileSync('Docs/yes24Data.json', 'utf-8');
 
 
 
@@ -60,40 +66,142 @@ app.use(morgan('dev'))
 
 
 
-app.use('/yes24', (req, res) => {
+app.use('/api', async (req, res, next) => {
     //logName();
-    //fs.readFile(`./Docs/yes24Data-${fileName}.json`, (err, data) => {
-    console.log("들어오는 요청", req.body);
+    let query = req.body['searchData'];
+    console.log(query);
+    // console.log("들어오는 요청1", req.body);
+
+    // const listFolder = 'Docs';
+    // const fileNameList = fs.readdirSync(listFolder);
+
+    // let fileIs = fileNameList.includes('booksApi.json');
+
+    // if (fileIs === true) {
+
+    //     fs.unlink("Docs/booksApi.json", async (err) => {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //         //   console.log('파일 존재 삭제처리');
+    //         console.log('삭제완료');
+    await dataHandler.booksQueryData(query);
+    next();
+    //     });
+    // } else {
+    //     console.log('파일 없음');
+    //     await dataHandler.booksQueryData(query);
+    //     next();
+    // }
+
+
+
 
     // 클라이언트에서 오는 검색어 -> 예) 자바스크립트
 
-    let query = req.body;
 
+    // console.log("b", b);
 
+    // if ((fileLenAl.length === 0) && (fileLen24.length === 0)) {
+    //     // await dataHandler.booksQueryData(query);
 
-    getQueryFromClient(query);
+    // }
+    // if ((fileLenAl.length !== 0) && (fileLen24.length !== 0)) {
+    //     next();
+    //     //     }
+    //     //     
+    //     // }
 
+    // }
 
-    // 프런트 보내는지 확인을 위해 임의의 파일
-    fs.readFile(`Docs/aladinApi.json`, (err, data) => {
-        if (err) throw err;
-        data = JSON.parse(data);
-        res.send(data);
-    })
-
-})
-
-
-app.get('/', (req, res) => {
-    res.send('Hello, World');
 });
 
 
+// let queryName = exports.queryName = function (query) {
+//     console.log("들어오는 요청2", query);
+// }
+
+
+app.post('/api', (req, res, next) => {
+
+    fs.readFile('Docs/yes24Data.json', (err, data1) => {
+        //  console.log("검색1");
+        let yes24 = JSON.parse(data1);
+        console.log("yes24길이 [{}] 형태이므로 : 20", yes24.length);
+        fs.readFile('Docs/aladinApi.json', (err, data2) => {
+            // console.log('검색2');
+            let aladin = JSON.parse(data2);
+            console.log("알라딘 길이 : 1", aladin[0].length);
+            let plusFile = aladin.slice();
+            plusFile.push(yes24);
+            console.log("합산배열길이: 항상2여야 할 것", plusFile.length);
+            fs.writeFile("Docs/dummyApi.json", JSON.stringify(plusFile), (err) => console.error(err));
+            fs.writeFile("Docs/booksApi.json", JSON.stringify(plusFile), function (err) {
+                if (err)
+                    console.error(err);
+                else {
+                    //      console.log('생성');
+                    fs.readFile("Docs/booksApi.json", (err, data) => {
+                        if (err) throw err;
+                        data = JSON.parse(data);
+                        res.send(data);
+                        removeContentFile();
+
+                    })
+                }
+            });
+        })
+    });
+});
+
+
+
+const removeContentFile = () => {
+    console.log("파일확인하기1234");
+    // fs.open('Docs/yes24Data.json', 'w', function (err, f) {
+    //     if (err) {
+    //         return console.error(err);
+    //     }
+    // })
+    // fs.open('Docs/aladinApi.json', 'w', function (err, f) {
+    //     if (err) {
+    //         return console.error(err);
+    //     }
+    // })
+    // fs.open('Docs/booksApi.json', 'w', function (err, f) {
+    //     if (err) {
+    //         return console.error(err);
+    //     }
+    // })
+    fs.unlink("Docs/aladinApi.json", async (err) => {
+        if (err) {
+            console.log(err);
+        }
+        //   console.log('파일 존재 삭제처리');
+        console.log('삭제완료')
+    })
+
+    fs.unlink("Docs/yes24Data.json", async (err) => {
+        if (err) {
+            console.log(err);
+        }
+        //   console.log('파일 존재 삭제처리');
+        console.log('삭제완료')
+    })
+
+    fs.unlink("Docs/booksApi.json", async (err) => {
+        if (err) {
+            console.log(err);
+        }
+        //   console.log('파일 존재 삭제처리');
+        console.log('삭제완료')
+    })
+
+}
 app.set('port', process.env.PORT || 5000);
 
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
-
 
